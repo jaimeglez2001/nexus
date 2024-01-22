2091ba07017f8e85234ef5491a6dd9372090b132
Jaime González <jaime01glez@gmai.com>
2024-01-22 19:56:46 +0100

    main

--- src/components/HerramientaDes.svelte
+++ src/components/HerramientaDes.svelte
@@ -16,14 +16,6 @@
     }
   }
 
-  function clear() {
-    firstRow = "";
-    secondRow = "";
-    thirdRow = "";
-    fourthRow = "";
-    count = 0;
-  }
-
   function volverAtras() {
     if (HUELLA.length > 0 && count > 0) {
       HUELLA.pop();
@@ -70,7 +62,7 @@
         count++;
       }
     }
-
+    
     console.log(count);
     console.log(firstRow);
     console.log(secondRow);
@@ -80,13 +72,13 @@
 </script>
 
 <section class="flex flex-col gap-40 items-center">
-  <article class="ctos flex gap-10">
-    <button on:click={clear} class="flex gap-4 items-center"> Limpiar</button>
+  <article class="ctos flex gap-10 pt-40">
     <button on:click={volverAtras} class="flex gap-4 items-center">
+      <figure><img src="./public/arrow-left.svg" alt="Flecha de Volver"></figure>
       Volver Atrás</button
     >
   </article>
-
+<section class="flex">
   <h4
     class="line_1 animated text-[--nexusBlue] text-[300px] leading-[60px] tracking-[-120px]"
     style="white-space: pre-line;"
@@ -98,19 +90,29 @@
   </h4>
 </section>
 
+</section>
+
+{#if count !== 9}
+
 <h1 class="text-3xl mb-6 mt-40">{etapas[count].pregunta}</h1>
 <h3 class="text-gray-400 text-center max-w-[1024px] text-xl mb-10">
   {etapas[count].ejemplo}
 </h3>
 <article class="ctas flex gap-10 mb-20">
-  <button on:click={() => renderHuella(0)} class="p-4 text-xl border-2"
+  <button on:click={() => renderHuella(0)} class="px-8 py-4 border-black text-xl border-2 hover:bg-[--nexusBlue] hover:text-white duration-200 hover:border-[--nexusBlue]"
     >{etapas[count].respuesta[0]}</button
   >
-  <button on:click={() => renderHuella(1)} class="p-4 text-xl border-2"
+  <button on:click={() => renderHuella(1)} class="px-8 py-4 border-black text-xl border-2 hover:bg-[--nexusBlue] hover:text-white duration-200 hover:border-[--nexusBlue]"
     >{etapas[count].respuesta[1]}</button
   >
-  <button on:click={() => renderHuella(2)} class="p-4 text-xl border-2"
+  <button on:click={() => renderHuella(2)} class="px-8 py-4 border-black text-xl border-2 hover:bg-[--nexusBlue] hover:text-white duration-200 hover:border-[--nexusBlue]"
     >{etapas[count].respuesta[2]}</button
   >
-  <button on:click={renderVacio} class="p-4 text-xl border-2">Ninguno</button>
+  <button on:click={renderVacio} class="px-8 py-4 border-black text-xl border-2 hover:bg-[--nexusBlue] hover:text-white duration-200 hover:border-[--nexusBlue]">{etapas[count].respuesta[3]}</button>
+
 </article>
+{/if}
+
+{#if count === 9}
+<h1 class="text-3xl mb-6 mt-40">Esta es tu huella</h1>
+{/if}
